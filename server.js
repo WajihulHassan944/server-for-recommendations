0557fe906b057debb69a4452e8f4ee9d76d44637
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2024-10-13 21:03:18 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const { ObjectId } = require('mongodb');
+const cors = require("cors");
+const FormData = require('form-data');
+
+app.use(express.json());
+app.use(cors());
+
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+
+// MongoDB connection
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+// Create a transporter object for sending the email
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'vascularbundle43@gmail.com',
+    pass: 'gxauudkzvdvhdzbg',
+  },
+});
+
+
+
+
+
+
+app.get("/", (req,res) =>{
+  res.send("Backend server for recommendations has started running successfully...");
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+  
